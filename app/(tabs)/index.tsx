50cf30e5b550cf456c4056f6f2dcafdec926b33a
Roy-ElHaddad <Royhaddad27@gmail.com>
2024-06-29 12:44:35 +0200

    integrated expo router and tabs for better app navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,8 @@
 import Colors from '@/assets/Colors/Colors'
 import Card from '@/components/Card'
 import data from '@/data/Posts.json'
-import { MaterialIcons } from '@expo/vector-icons'
 import React, { useEffect, useState } from 'react'
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native'
+import { FlatList, StyleSheet, View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 type Post = {
@@ -32,7 +25,6 @@ export default function App() {
 	const handleDelete = (id) => {
 		const updatedPosts = posts.filter((post) => post.id !== id)
 		setPosts(updatedPosts)
-		// alert(`Delete ${id}`)
 	}
 
 	renderPost = (post) => {
@@ -53,12 +45,6 @@ export default function App() {
 					backgroundColor: Colors.background,
 				}}
 			>
-				<View style={styles.header}>
-					<Text style={styles.title}>Feed</Text>
-					<TouchableOpacity onPress={() => alert('adding post')}>
-						<MaterialIcons name="post-add" size={24} color="black" />
-					</TouchableOpacity>
-				</View>
 				<FlatList
 					style={styles.feedlist}
 					data={posts}
@@ -72,27 +58,6 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-	header: {
-		width: '100%',
-		paddingTop: 20,
-		paddingBottom: 16,
-		backgroundColor: Colors.white,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		paddingHorizontal: 15,
-		borderBottomWidth: 1,
-		borderBlockColor: Colors.border,
-		shadowColor: Colors.shadow,
-		shadowOffset: { width: 0, height: 5 },
-		shadowRadius: 15,
-		shadowOpacity: 0.2,
-		zIndex: 10,
-	},
-	title: {
-		fontSize: 24,
-		fontWeight: 'bold',
-	},
 	feedlist: {
 		padding: 16,
 	},
---
+++ app/(tabs)/posts/new.tsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native"
+
+export default function NewPost(){
+    return <View>
+        <Text>Test</Text>
+    </View>
+}
